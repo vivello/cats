fb8a1d0f05e979176729ed7285f97e23bd8a939a
Vivian Huang <vivello@Vivians-MacBook-Air.local>
2022-08-27 03:14:31 -0400

    Make rage mostly stable

--- src/Components/Cat.js
+++ src/Components/Cat.js
@@ -1,46 +1,107 @@
 // import '../../App.css';
 import React, { Component } from "react";
 import NeutralCatImage from '../Images/cat-placeholder.png'
+import HappyCatImage from '../Images/purr-24.png'
+import AnnoyedCatImage from '../Images/purr-robot-vacuum-cleaner-1.png'
 
 
 class Cat extends Component {
     constructor() {
       super();
       this.state = {
-          mood: 'neutral'
+          mood: 'neutral',
+          rage: 0
       };
     }
   
     loadSprite = () => {
       if (this.state.mood === 'neutral') {
         return NeutralCatImage
+      } else if (this.state.mood === 'happy') {
+        return HappyCatImage
+      } else {
+        return AnnoyedCatImage
       }
     };
 
     checkForLoss = () => {
-        let random = Math.random() * 40
-        if (random < 2) {
+        if (this.state.rage > 300) {
             this.props.lose() 
             return true;
         }
         return false;
     }
 
+    incRage = (amt) => {
+        if (amt > 0) {
+            let random = Math.random() * 50
+            if (random < 5) {
+                this.setState({
+                    mood: 'annoyed'
+                })
+            } else if (random < 35) {
+                this.setState({
+                    mood: 'neutral'
+              })
+            } else {
+                this.setState({
+                    mood: 'happy'
+              })
+        }
+      }
+      if (this.state.rage < 0) {
+          clearInterval(this.state.rageIntervalId)
+          this.setState({
+            rage: 0
+          })
+      } else {
+          this.setState((prevState) => {
+              return {rage: prevState.rage + amt}
+        });
+      }
+    }
+
     handleMouseEnter = () => {
+        let relaxIntervalId = setInterval(() => {
+        }, 10);
+        this.setState({ relaxIntervalId: relaxIntervalId}) 
+        clearInterval(this.state.relaxIntervalId)
         let scoreIntervalId = setInterval(() => {
+                this.props.scorePoints()
+        }, 500);
+        this.setState({ scoreIntervalId: scoreIntervalId });
+        let rageIntervalId = setInterval(() => {
             let isLoss = this.checkForLoss()
-            if (isLoss === true) {
+            if (isLoss == true) {
+                this.setState({
+                    mood: 'neutral',
+                    rage: 0
+                })
                 clearInterval(this.state.scoreIntervalId)
+                clearInterval(this.state.rageIntervalId)
                 return;
             } else {
-                this.props.scorePoints()
+                if (this.state.mood === 'happy') {
+                    this.incRage(10)
+                } else if (this.state.mood === 'neutral') {
+                    this.incRage(30)
+                } else {
+                    this.incRage(60)
+                }
             }
-        }, 500);
-        this.setState({ scoreIntervalId: scoreIntervalId })
+        }, 750);
+        this.setState({ rageIntervalId: rageIntervalId});
     }
 
     handleMouseLeave = () => {
         clearInterval(this.state.scoreIntervalId)
+        clearInterval(this.state.rageIntervalId)
+        if (this.state.rage > 0) {
+            let relaxIntervalId = setInterval(() => {
+                this.incRage(-20)
+          } , 200); 
+          this.setState({ relaxIntervalId: relaxIntervalId})
+        }
     }
     
 
